b32780102f5a32790f3fea4fd6ddb1edf2a04776
menqiqi <39701053+menqiqi@users.noreply.github.com>
2018-12-03 23:48:32 +0800

    移除元素

---
+++ Practice27.java
@@ -0,0 +1,28 @@
+/**
+ * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ */
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+public class Practice27 {
+    public static void main(String[] args) {
+        int[] A = new int[]{3,2,2,3};
+        Solution solution = new Solution();
+        System.out.println(solution.removeElement(A,3));
+    }
+}
