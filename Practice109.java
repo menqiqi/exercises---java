02bb604f7beea1744ce6212dd1b5e38cbe822a75
menqiqi <2251048927@qq.com>
2019-02-09 20:46:39 +0800

    最接近原点的K个点

---
+++ Practice109.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+/**
+ *  我们有一个由平面上的点组成的列表 points。需要从中找出 K 个距离原点 (0, 0) 最近的点。
+ *
+ * （这里，平面上两点之间的距离是欧几里德距离。）
+ *
+ * 你可以按任何顺序返回答案。除了点坐标的顺序之外，答案确保是唯一的。
+ *
+ *
+ *
+ * 示例 ：
+ *
+ * 输入：points = [[1,3],[-2,2]], K = 1
+ * 输出：[[-2,2]]
+ * 解释：
+ * (1, 3) 和原点之间的距离为 sqrt(10)，
+ * (-2, 2) 和原点之间的距离为 sqrt(8)，
+ * 由于 sqrt(8) < sqrt(10)，(-2, 2) 离原点更近。
+ * 我们只需要距离原点最近的 K = 1 个点，所以答案就是 [[-2,2]]。
+ */
+
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        int N = points.length;
+        int[] dists = new int[N];
+        for (int i = 0; i < N; ++i)
+            dists[i] = dist(points[i]);
+
+        Arrays.sort(dists);
+        int distK = dists[K-1];
+
+        int[][] ans = new int[K][2];
+        int t = 0;
+        for (int i = 0; i < N; ++i)
+            if (dist(points[i]) <= distK)
+                ans[t++] = points[i];
+        return ans;
+    }
+
+    public int dist(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
+}
+
+public class Practice109 {
+    public static void main(String[] args) {
+        int[][] data = {{1,3},{-2,2}};
+        Solution solution = new Solution();
+        int[][] res = solution.kClosest(data,1);
+        for (int i = 0; i < res.length; i++){
+            System.out.println(res[i][0]+","+res[i][1]);
+        }
+    }
+}
