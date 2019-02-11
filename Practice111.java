196578250b39d15d6e34bdf80b27ee45a895c022
menqiqi <2251048927@qq.com>
2019-02-11 21:23:39 +0800

    查询后的偶数和

---
+++ Practice111.java
@@ -0,0 +1,54 @@
+/**
+ * 给出一个整数数组 A 和一个查询数组 queries。
+ *
+ * 对于第 i 次查询，有 val = queries[i][0], index = queries[i][1]，我们会把 val 加到 A[index] 上。然后，第 i 次查询的答案是 A 中偶数值的和。
+ *
+ * （此处给定的 index = queries[i][1] 是从 0 开始的索引，每次查询都会永久修改数组 A。）
+ *
+ * 返回所有查询的答案。你的答案应当以数组 answer 给出，answer[i] 为第 i 次查询的答案。
+ *
+ *
+ *
+ * 示例：
+ *
+ * 输入：A = [1,2,3,4], queries = [[1,0],[-3,1],[-4,0],[2,3]]
+ * 输出：[8,6,2,4]
+ * 解释：
+ * 开始时，数组为 [1,2,3,4]。
+ * 将 1 加到 A[0] 上之后，数组为 [2,2,3,4]，偶数值之和为 2 + 2 + 4 = 8。
+ * 将 -3 加到 A[1] 上之后，数组为 [2,-1,3,4]，偶数值之和为 2 + 4 = 6。
+ * 将 -4 加到 A[0] 上之后，数组为 [-2,-1,3,4]，偶数值之和为 -2 + 4 = 2。
+ * 将 2 加到 A[3] 上之后，数组为 [-2,-1,3,6]，偶数值之和为 -2 + 6 = 4。
+ */
+
+class Solution {
+    public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+        int[] answer = new int[A.length];
+        int k = 0;
+        for(int i = 0; i < queries.length; i++){
+            //需要查询的次数
+            int val = queries[i][0];
+            int index = queries[i][1];
+            A[index] = val + A[index];
+            //计算出A数组中的偶数和
+            int sum = 0;
+            for(int j = 0; j < A.length; j++){
+                if(A[j] % 2 == 0)
+                    sum = sum + A[j];
+            }
+            answer[k++] = sum;
+        }
+        return answer;
+    }
+}
+
+public class Practice111 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] A = {1,2,3,4};
+        int[][] queries = {{1,0},{-3,1},{-4,0},{2,3}};
+        int[] answer = solution.sumEvenAfterQueries(A,queries);
+        for (int tmp : answer)
+            System.out.print(tmp + "、");
+    }
+}
