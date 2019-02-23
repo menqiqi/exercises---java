0b9327ba198b22db044b5de0037f3a5d8e610437
menqiqi <39701053+menqiqi@users.noreply.github.com>
2019-02-23 16:44:03 +0800

    Update Practice136.java

--- Practice136.java
+++ Practice136.java
@@ -1,3 +1,9 @@
+/**
+ * 六一儿童节，老师带了很多好吃的巧克力到幼儿园。
+ * 每块巧克力j的重量为w[j]，对于每个小朋友i，当他分到的巧克力大小达到h[i] (即w[j]>=h[i])，他才会上去表演节目。
+ * 老师的目标是将巧克力分发给孩子们，使得最多的小孩上台表演。
+ * 可以保证每个w[i]> 0且不能将多块巧克力分给一个孩子或将一块分给多个孩子。
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
