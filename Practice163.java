3f6086437a3baccba8b22fb60cc99bc0f3645160
menqiqi <2251048927@qq.com>
2019-03-11 20:51:44 +0800

    二叉搜索树中第k小的元素

---
+++ Practice163.java
@@ -0,0 +1,41 @@
+/**
+ * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+ *
+ * 说明：
+ * 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+ *
+ * 示例 :
+ *
+ * 输入: root = [3,1,4,null,2], k = 1
+ *    3
+ *   / \
+ *  1   4
+ *   \
+ *    2
+ * 输出: 1
+ */
+
+//二叉搜索树的中序遍历结果就是升序
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        inOrder(list,root);
+        return list.get(k-1);
+    }
+
+    private static void inOrder(List<Integer>list,TreeNode root){
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            TreeNode top = stack.pop();
+            list.add(top.val);
+            cur = top.right;
+        }
+    }
+}
+
