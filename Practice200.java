ecb95d00bf32faf70ebce98e3dede5229617ab1e
menqiqi <2251048927@qq.com>
2019-03-25 19:07:26 +0800

    成绩排序

--- Practice200.java
+++ Practice200.java
@@ -1,39 +1,67 @@
 import java.util.*;
-import java.util.Map.Entry;
 
 /**
  * 输入按成绩进行排序。
- *
+ * <p>
  * 输入描述：
  * 第一行输入学生个数，第二行输入规则，例如0代表降序输出，后面输入每个学生的姓名和成绩
- *
+ * <p>
  * 输出描述：
  * 按照一定规则输出学生的姓名和成绩
  */
-public class Practice200 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()){
-            int n = sc.nextInt();                  //接收学生数量
-            Map<Integer,Integer> map = new TreeMap<>();   //创建treemap存储数据
-            for(int i=0;i<n;i++){                         //接收学生学号及成绩并存入treemap
-                map.put(sc.nextInt(),sc.nextInt());
-            }                                           //将treemap转成list存储
-            List<Map.Entry<Integer,Integer>> list = new ArrayList(map.entrySet());
-            //将list进行排序，用了一个匿名内部类定义比较器
-            Collections.sort(list,new Comparator<Map.Entry<Integer,Integer>>(){
-                public int compare(Entry<Integer,Integer> cp1,Entry<Integer,Integer> cp2){
-                    if(cp2.getValue() > cp1.getValue()){
-                        return -1;
-                    }else if(cp2.getValue().equals(cp1.getValue())){  //此else可以其实可以不写的
-                        return cp2.getKey() > cp1.getKey() ? -1: 1;   //因为treemap默认是键升序排列
+
+public class Practice200
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        while(scanner.hasNext())
+        {
+            int numPeople=scanner.nextInt();
+            int option=scanner.nextInt();
+
+            List<Student> stuList=new ArrayList<Student>();
+            for(int i=0;i<numPeople;i++)
+            {
+                stuList.add(new Student(scanner.next(), scanner.nextInt()));
+            }
+
+            //降序
+            if(option==0)
+            {
+                Collections.sort(stuList, new Comparator<Student>()
+                {
+                    public int compare(Student o1, Student o2)
+                    {
+                        return o2.score-o1.score;
                     }
-                    return 1;
-                }
-            });
-            for(Map.Entry<Integer, Integer> mapping : list){
-                System.out.println(mapping.getKey()+" "+mapping.getValue());
+                });
+            }
+            else if(option==1)//升序
+            {
+                Collections.sort(stuList, new Comparator<Student>()
+                {
+                    public int compare(Student o1, Student o2)
+                    {
+                        return o1.score-o2.score;
+                    }
+                });
+            }
+            for(int i=0;i<stuList.size();i++)
+            {
+                System.out.println(stuList.get(i).name+" "+stuList.get(i).score);
             }
         }
+    }
+}
+
+class Student
+{
+    public String name;
+    public int score;
+    public Student(String name,int score)
+    {
+        this.name=name;
+        this.score=score;
     }
 }
