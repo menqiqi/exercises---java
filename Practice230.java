906e57f82ffa081e4a2075b1e8f1471454d3df34
menqiqi <2251048927@qq.com>
2019-04-09 09:43:55 +0800

    说反话

--- Practice230.java
+++ Practice230.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 /**
- * K个一组反转链表
+ * 说反话
  *
- * Path/XPath解析成规则的十进制数值
+ * 给定一句英语，要求你编写程序，将句中所有的单词颠倒输出。
  */
 
 public class Practice230 {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()){
+            String[] strings = in.nextLine().split(" ");
+            int left = 0;
+            int right = strings.length-1;
+            while (left < right){
+                String tmp = strings[left];
+                strings[left] = strings[right];
+                strings[right] = tmp;
+                left++;
+                right--;
+            }
+            for (String s : strings)
+                System.out.print(s+" ");
+        }
+        in.close();
     }
+
+
 }
