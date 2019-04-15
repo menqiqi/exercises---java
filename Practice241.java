d9a485837cdd4eb83699cff6f21d28a5f1e340df
menqiqi <2251048927@qq.com>
2019-04-15 23:00:02 +0800

    汽水瓶

---
+++ Practice241.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 有这样一道智力题：“某商店规定：三个空汽水瓶可以换一瓶汽水。
+ * 小张手上有十个空汽水瓶，她最多可以换多少瓶汽水喝？”答案是5瓶，
+ * 方法如下：先用9个空瓶子换3瓶汽水，喝掉3瓶满的，喝完以后4个空瓶子，用3个再换一瓶，喝掉这瓶满的，
+ * 这时候剩2个空瓶子。然后你让老板先借给你一瓶汽水，喝掉这瓶满的，喝完以后用3个空瓶子换一瓶满的还给老板。
+ * 如果小张手上有n个空汽水瓶，最多可以换多少瓶汽水喝？
+ */
+
+public class Practice241 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n;
+        while (in.hasNext()){
+            n = in.nextInt();
+            if (n == 0)
+                break;
+            System.out.println(drink(n));
+        }
+        in.close();
+    }
+
+    private static int drink(int n){
+        if (n == 0){
+            return 0;
+        } else if (n == 3){
+            return 1;
+        } else if (n == 2){
+            return 1;
+        }else {
+            //3的倍数
+            int tmp = n / 3;
+            return tmp + drink(n-(tmp*3)+tmp);
+        }
+
+    }
+}
